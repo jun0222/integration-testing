44338e35262ce1d789d67636cac72ac5d80f8815
JunHatanaka <nextseason6@gmail.com>
2025-11-13 23:46:42 +0900

    feature(selenium): path

--- selenium-webdriver/test_purchase_flow.py
+++ selenium-webdriver/test_purchase_flow.py
@@ -1,8 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
+from pathlib import Path
 
-BASE_URL="/selenium-webdriver/mock_site/index.html"
+BASE_URL = Path.cwd().joinpath("selenium-webdriver/mock_site/index.html").as_uri()
 
 # ブラウザを起動
 driver_chrome = webdriver.Chrome()
